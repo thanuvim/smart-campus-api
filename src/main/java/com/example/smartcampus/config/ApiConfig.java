8bb83cc23745f25091bccc6c8482b7b753085bb5
thanuvim <thanuvimuthukumarana@gmail.com>
2026-04-24 02:18:59 +0530

    commiting ApiConfig

---
+++ src/main/java/com/example/smartcampus/config/ApiConfig.java
@@ -0,0 +1,8 @@
+package com.example.smartcampus.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class ApiConfig extends Application {
+}
