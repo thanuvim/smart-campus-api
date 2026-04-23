5c7e5ddd66e2a52522366c7a4344418176f6623b
thanuvim <thanuvimuthukumarana@gmail.com>
2026-04-24 02:22:28 +0530

    Create custom runtime exception for cases where referenced resources don't exist

---
+++ src/main/java/com/example/smartcampus/exception/LinkedResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.smartcampus.exception;
+
+public class LinkedResourceNotFoundException extends RuntimeException {
+
+    public LinkedResourceNotFoundException(String message) {
+        super(message);
+    }
+}
