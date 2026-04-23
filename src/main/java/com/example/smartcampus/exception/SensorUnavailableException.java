230bc623543718d9b0f82c9feae152f191e44b31
thanuvim <thanuvimuthukumarana@gmail.com>
2026-04-24 02:28:14 +0530

    Create custom runtime exception for sensor unavailability

---
+++ src/main/java/com/example/smartcampus/exception/SensorUnavailableException.java
@@ -0,0 +1,8 @@
+package com.example.smartcampus.exception;
+
+public class SensorUnavailableException extends RuntimeException {
+
+    public SensorUnavailableException(String message) {
+        super(message);
+    }
+}
